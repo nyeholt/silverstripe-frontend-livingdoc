2d6e959772d434ec48777e8ce2e72c2830937634
Marcus Nyeholt <marcus@symbiote.com.au>
2019-10-21 22:24:39 +1100

    new(webpack) Added copying of static assets for theme

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,6 +1,7 @@
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 
 module.exports = {
@@ -75,6 +76,21 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
-        })
+        }),
+        new CopyWebpackPlugin(
+            [
+                {
+                    from: './assets',
+                    to: './',
+                }
+            ],
+            {
+                ignore: [
+                    'images/icons/*',
+                    'fonts/*',
+                ],
+                copyUnmodified: true
+            }
+        )
     ]
 };
