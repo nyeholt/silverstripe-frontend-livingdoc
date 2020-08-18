83d685a35850b2c87f15b36e3808263e145ef873
Marcus Nyeholt <marcus@symbiote.com.au>
2020-08-18 23:33:37 +1000

    new(ComponentPage) Include css for component pages in draft mode

---
+++ src/Page/ComponentPageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Symbiote\Frontend\LivingPage\Page;
+
+use PageController;
+use SilverStripe\Versioned\Versioned;
+use SilverStripe\View\Requirements;
+
+class ComponentPageController extends PageController
+{
+    public function doInit()
+    {
+        // if we're in 'stage' mode, and a living docs page, grab its
+        parent::doInit();
+
+        if (Versioned::get_stage() == Versioned::DRAFT) {
+            Requirements::css('nyeholt/silverstripe-frontend-livingdoc: app/dist/main.css');
+        }
+    }
+}
